080aa94ed9a4882baa9f427676a4b17bf301281e
ThomasHovland <hovlandthomas1@gmail.com>
2024-10-24 10:47:58 +0200

    java script

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('Hello!');
+console.log('Welkomme!');
+
+//  Variables
+
+// Let variable saving the number 28 on the name age
+let age = 19;
+console.log(age)
+console.log(typeof age)
+
+// Const variable saving the string Thomas on the name (name)
+const name = "Thomas";
+console.log(name)
+console.log(typeof name)
+
+// Const variable saving the boolean true on the name isPresent
+const isPresent = true;
+console.log(isPresent)
+console.log(typeof isPresent)
