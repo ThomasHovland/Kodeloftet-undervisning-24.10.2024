d5729a5e425d2cf917bdd51b0d644209735692c1
ThomasHovland <hovlandthomas1@gmail.com>
2024-10-24 12:00:26 +0200

    js

--- script.js
+++ script.js
@@ -4,15 +4,32 @@ console.log('Welkomme!');
 
 // Let variable saving the number 28 on the name age
 let age = 19;
-console.log(age)
+// Chaining method:
+console.log("min alder er:", age)
+// Teplate litteral
+console.log(`Min alder er: ${age}`)
 console.log(typeof age)
 
 // Const variable saving the string Thomas on the name (name)
 const name = "Thomas";
-console.log(name)
+console.log("mit navn er:", name)
 console.log(typeof name)
 
 // Const variable saving the boolean true on the name isPresent
 const isPresent = true;
-console.log(isPresent)
+console.log("Er tilstede?", isPresent)
 console.log(typeof isPresent)
+
+//
+//  skille mellom store deler med kode
+//
+
+// Two const variables saving strings 
+// One const variable saving the data from the two first variables
+const firstName = "Thomas";
+const lastName = "Hovland";
+const fullName = firstName + " " + lastName;
+console.log("fullname variable viser:", fullName)
+
+const htmlElement = document.querySelector("#java-script-data")
+htmlElement.textContent = fullName;
